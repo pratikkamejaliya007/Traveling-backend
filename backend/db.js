44c2ca1db27481b2c4c05b400468e3483cfb0ef6
pratikkamejaliya007 <pratikkamejaliya6157@gmail.com>
2024-11-28 18:16:13 +0530

    backend

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const db = mongoose.connect("mongodb://localhost:27017/traveling") 
+// mongodb+srv://pratikkamejaliya6157:<db_password>@cluster0.c12o5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const db = mongoose.connect("mongodb+srv://pratikkamejaliya6157:ovFhjjqO4l8v0ToJ@cluster0.c12o5.mongodb.net/?retryWrites=true&w=majority&appName=traveling") 
 .then(()=> console.log("MongoDb Connected"))
 .catch((err)=> console.error(err))
 
